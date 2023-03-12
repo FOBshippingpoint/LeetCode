36409ab33cb2053250585bbec5fd4b1a3ca1cdc1
FOBshippingpoint <aulaulql4ql4xj6@gmail.com>
2023-03-12 22:26:20 +0800

    加入專案檔案。

---
+++ LeetCode/0009_PalindromeNumber.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Date:    2023/0312
+ * Problem: 9. Palindrome Number
+ * Link:    https://leetcode.com/problems/palindrome-number/
+ */
+
+namespace LeetCode
+{
+    internal class PalindromeNumber
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+            string s = x.ToString();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char reverse = s[s.Length - 1 - i];
+                if (s[i] != reverse)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
+
