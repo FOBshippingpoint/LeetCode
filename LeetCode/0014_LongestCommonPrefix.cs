1b6387de49cf07c0c6912faa76bc2fb03024c89e
FOBshippingpoint <aulaulql4ql4xj6@gmail.com>
2023-03-15 00:03:50 +0800

    feat: longest common prefix

---
+++ LeetCode/0014_LongestCommonPrefix.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Date:    2023/03/14
+ * Problem: 14. Longest Common Prefix
+ * Link:    https://leetcode.com/problems/longest-common-prefix/
+ */
+
+namespace LeetCode
+{
+    class Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            // find the shortest str in strs
+            var shortest = "";
+            var shortestLen = 200;
+            foreach (string str in strs)
+            {
+                if (str.Length < shortestLen)
+                {
+                    shortestLen = str.Length;
+                    shortest = str;
+                }
+            }
+
+            int commonLen = FindCommonLength(shortest, strs);
+            return shortest.Substring(0, commonLen);
+        }
+
+        private int FindCommonLength(string str, string[] strs)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                char c = str[i];
+                foreach (string s in strs)
+                {
+                    if (s[i] != c)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return str.Length;
+        }
+    }
+}
+
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using LeetCode;
+
+
+var s = new Solution();
+Console.WriteLine(s.LongestCommonPrefix(new string[] { "dog", "racecar", "car" }));
