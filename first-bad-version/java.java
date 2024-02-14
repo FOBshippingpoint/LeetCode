ea8d25fb0afda8093b52dfea034f9fbdf6c9d588
CC Lan <60565355+FOBshippingpoint@users.noreply.github.com>
2024-02-14 15:24:13 +0800

    https://leetcode.com/problems/first-bad-version/submissions/1174907501

---
+++ first-bad-version/java.java
@@ -0,0 +1,19 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        var left = 1;
+        var right = n;
+
+        while (left <= right) {
+            var mid = right + (left - right) / 2;
+            if (isBadVersion(mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
