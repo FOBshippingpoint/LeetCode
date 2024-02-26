a36054b76d22dde1d53c70b2cc07fdc9a3724c6a
CC Lan <60565355+FOBshippingpoint@users.noreply.github.com>
2024-02-26 21:34:58 +0800

    https://leetcode.com/problems/k-closest-points-to-origin/submissions/1186749045

--- k-closest-points-to-origin/java.java
+++ k-closest-points-to-origin/java.java
@@ -2,33 +2,17 @@ import java.util.PriorityQueue;
 
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        var queue = new PriorityQueue<HeapElement>();
+        var queue = new PriorityQueue<int[]>((p1, p2) -> (p1[0] * p1[0] + p1[1] * p1[1]) - (p2[0] * p2[0] + p2[1] * p2[1]));
 
         for (var point : points) {
-            var distance = point[0] * point[0] + point[1] * point[1];
-            queue.add(new HeapElement(distance, point));
+            queue.offer(point);
         }
 
         var result = new int[k][2];
-        for (var i = 0; i < k; i++) {
-            result[i] = queue.poll().point;
+        while (k > 0) {
+            result[--k] = queue.poll();
         }
 
         return result;
-    }
-
-    class HeapElement implements Comparable<HeapElement> {
-        public int distance;
-        public int[] point;
-
-        public HeapElement(int distance, int[] point){
-            this.distance = distance;
-            this.point = point;
-        }
-
-        @Override
-        public int compareTo(HeapElement other) {
-            return this.distance - other.distance;
-        }
     }
 }
