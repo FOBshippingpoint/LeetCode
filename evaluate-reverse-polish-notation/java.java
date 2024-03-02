1f39a47558d12d3a47bf43b70dd3eeefa5551c0d
CC Lan <60565355+FOBshippingpoint@users.noreply.github.com>
2024-03-03 03:11:26 +0800

    https://leetcode.com/problems/evaluate-reverse-polish-notation/submissions/1191915437

---
+++ evaluate-reverse-polish-notation/java.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        var stack = new Stack<Integer>();
+
+        for (var token : tokens) {
+            int a, b;
+            switch (token) {
+                case "+":
+                    b = stack.pop();
+                    a = stack.pop();
+                    stack.push(a + b);
+                    break;
+                case "-":
+                    b = stack.pop();
+                    a = stack.pop();
+                    stack.push(a - b);
+                    break;
+                case "*":
+                    b = stack.pop();
+                    a = stack.pop();
+                    stack.push(a * b);
+                    break;
+                case "/":
+                    b = stack.pop();
+                    a = stack.pop();
+                    stack.push(a / b);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(token));
+                    break;
+            }
+        }
+
+        return stack.pop();
+    }
+}
